538d75db90cabbfd978de28713fda1de12f14c10
Andrii Zaimak <andreyzaymak@gmail.com>
2022-04-04 16:19:32 +0300

    Добавлен чекбокс с отпракой на сервер

--- src/components/todos/TodoItem.tsx
+++ src/components/todos/TodoItem.tsx
@@ -1,15 +1,15 @@
-import React, { ChangeEvent, FunctionComponent, useState } from 'react';
+import React, { FunctionComponent } from 'react';
+import { useAppDispatch } from '../../hook/useAppDispatch';
 import { ITodo } from '../../models/todo';
+import { toggleTodo } from '../../store/todosSlice';
 import './style.scss';
 
 const TodoItem: FunctionComponent<ITodo> = ({ id, completed, title }) => {
-  const [isChecked, setIsChecked] = useState(completed);
-  const toggleHandler = (evt: ChangeEvent<HTMLInputElement>) => {
-    setIsChecked((prev) => !prev);
-  };
+  const dispatch = useAppDispatch();
+
   return (
-    <li key={id} className={isChecked ? 'checked' : ''}>
-      <input type={'checkbox'} checked={isChecked} onChange={toggleHandler} />
+    <li key={id} className={completed ? 'checked' : ''}>
+      <input type={'checkbox'} checked={completed} onChange={() => dispatch(toggleTodo({ id }))} />
       {id}. {title}
     </li>
   );
--- src/store/todosSlice.ts
+++ src/store/todosSlice.ts
@@ -1,3 +1,4 @@
+import { RootState } from './index';
 import { ITodo } from './../models/todo';
 import { createSlice, createAsyncThunk, CaseReducer, PayloadAction } from '@reduxjs/toolkit';
 
@@ -17,21 +18,22 @@ const initialState: TodosSliceType = {
   status: 'idle',
   error: null,
 };
-// Тип данных для получения списка дел
-type FetchTodosType = {
-  limit: number;
-  page: number;
-};
 
+// Запись ошибки
 const setError: CaseReducer<TodosSliceType, PayloadAction<string | null>> = (state, action) => {
   state.status = 'failed';
   state.error = action.payload;
 };
 
+// Тип данных для получения списка дел
+type FetchTodosType = {
+  limit: number;
+  page: number;
+};
 // Получение списка дел от сервера
 export const fetchTodos = createAsyncThunk<ITodo[], FetchTodosType, { rejectValue: string }>(`${SLICE_NAME}/fetchTodos`, async ({ limit, page }, { rejectWithValue }) => {
   try {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/todos1?_page=${page}&_limit=${limit}`);
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos?_page=${page}&_limit=${limit}`);
 
     // Check for response status
     if (!response.ok) throw new Error(`Server error! Status code: ${response.status}`);
@@ -43,11 +45,46 @@ export const fetchTodos = createAsyncThunk<ITodo[], FetchTodosType, { rejectValu
   }
 });
 
+// Toggle todo
+export const toggleTodo = createAsyncThunk<undefined, { id: number }, { rejectValue: string; state: RootState }>(
+  `${SLICE_NAME}/toggleTodo`,
+  async ({ id }, { rejectWithValue, getState, dispatch }) => {
+    try {
+      // Поиск todo по id в списке дел
+      const todo = getState().todos.todosList.find((todo: ITodo) => todo.id === id);
+
+      const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ completed: !todo?.completed }),
+      });
+
+      // Check for response status
+      if (!response.ok) throw new Error(`Server error! Method toggleTodo. Status code: ${response.status}`);
+
+      dispatch(toggleComplete({ id }));
+    } catch (error) {
+      return rejectWithValue((error as Error).message);
+    }
+  }
+);
+
 // Создание Slice
 const todosSlice = createSlice({
   name: SLICE_NAME,
   initialState: initialState,
-  reducers: {},
+  reducers: {
+    toggleComplete(state, action: PayloadAction<{ id: number }>) {
+      const toggledTodo = state.todosList.find((todo: ITodo) => todo.id === action.payload.id);
+
+      //Проверка на существоание todo
+      if (!toggledTodo) return;
+
+      toggledTodo.completed = !toggledTodo?.completed;
+    },
+  },
   extraReducers: {
     [fetchTodos.pending.type]: (state) => {
       state.status = 'loading';
@@ -61,5 +98,6 @@ const todosSlice = createSlice({
     [fetchTodos.rejected.type]: setError,
   },
 });
+export const { toggleComplete } = todosSlice.actions;
 
 export default todosSlice.reducer;
