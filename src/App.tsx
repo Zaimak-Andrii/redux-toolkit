6b87a9580c5151ede370d48aa6973202ab492c7f
Andrii Zaimak <andreyzaymak@gmail.com>
2022-04-11 10:28:01 +0300

    Добавлена ленивая загрузка компонентов, Кастомный suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import { LinearProgress } from '@mui/material';
+import React, { lazy, Suspense, ReactNode, FunctionComponent } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/layout/layout';
 import Homepage from './pages/home/Homepage';
 import Notfoundpage from './pages/Notfoundpage';
 import Photospage from './pages/Photospage';
 import Postspage from './pages/Postspage';
-import Todospage from './pages/todo/Todospage';
-import Userspage from './pages/users/Userspage';
+
+// Ленивая подгрузка компонента. Подгрузится только, когда перейдешь на ссылку
+const UsersPage = lazy(() => import('./pages/users/Userspage'));
+const TodosPage = lazy(() => import('./pages/todo/Todospage'));
+
+const CustomSuspense: FunctionComponent<{ children: ReactNode }> = ({ children }) => {
+  return <Suspense fallback={<LinearProgress />}>{children}</Suspense>;
+};
 
 function App() {
   return (
@@ -14,9 +21,31 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Homepage />} />
-          <Route path='users' element={<Userspage />} />
-          <Route path='todos' element={<Todospage />} />
-          <Route path='todos/:page' element={<Todospage />} />
+          <Route
+            path='users'
+            element={
+              // fallback - loader (react- component).
+              <CustomSuspense>
+                <UsersPage />
+              </CustomSuspense>
+            }
+          />
+          <Route
+            path='todos'
+            element={
+              <CustomSuspense>
+                <TodosPage />
+              </CustomSuspense>
+            }
+          />
+          <Route
+            path='todos/:page'
+            element={
+              <CustomSuspense>
+                <TodosPage />
+              </CustomSuspense>
+            }
+          />
           <Route path='photos' element={<Photospage />} />
           <Route path='posts' element={<Postspage />} />
           <Route path='*' element={<Notfoundpage />} />
